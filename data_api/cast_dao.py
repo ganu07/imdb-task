88e1507e6f0ba69e399b0c2760c423473aa812bb
ganu07 <gborhade7@gmail.com>
2021-12-22 06:54:10 +0530

    final commit

--- data_api/cast_dao.py
+++ data_api/cast_dao.py
@@ -7,25 +7,40 @@ from data_api.models import Cast
 
 
 class CastDao(object):
-    
+    """
+    A static Cast dao class to isolate cast related functionality
+    """
     @staticmethod
     def get_cast(session, name):
-        
-        pass
+        """
+        :param session: DB session to passed from caller
+        :param name: Cast name to be queried from db
+        :return: SQLAlchemy Cast object returned from DB
+        """
+        return session.query(Cast).filter(Cast.name == name).first()
 
     @staticmethod
     def add_cast(session, name):
-        
+        """
+        :param session: DB session to passed from caller
+        :param name: Cast name to be added in db
+        :return: Cast object of python class
+        """
         cast_obj = Cast(name)
-        
+        session.add(cast_obj)
         return cast_obj
 
     @staticmethod
     def check_or_add_cast(session, cast_name):
-       
+        """
+        :param session: DB session to passed from caller
+        :param cast_name: Cast name to be queried or added in db
+        :return: Cast object queried from db or created with id after session flush
+        """
         cast_obj = CastDao.get_cast(session, cast_name)
 
         if not cast_obj:
-            
+            cast_obj = CastDao.add_cast(session, cast_name)
+            session.flush()
         return cast_obj
 
--- webapp.py
+++ webapp.py
@@ -1,14 +1,22 @@
 from flask import Flask
 
-
+# from helpers.db import load_db
+# from helpers.parser import Parser
 from views.movies import blueprint
 
 
 def create_app():
+    # Create DB
+    # load_db()
+
+    # parser = Parser('imdb.json')
+    # parser.populate()
 
     # Instantiate flask app
     app = Flask(__name__)
-	app.run(debug=True)
+
+    # Register Blueprint
+    app.register_blueprint(blueprint)
 
     # Return app
     return app
